b00ac450124573c0b34575dcfbfa7d63d069154c
GamerNoTitle <GamerNoTitle@outlook.com>
2022-07-16 13:10:50 +0800

    No File Lock

--- utils/HexoLinkCheck.py
+++ utils/HexoLinkCheck.py
@@ -28,9 +28,11 @@ def get_link(url, ss=False):
     filename = 'hexo-link-check/' + url.replace('http://', '').replace('https://',
                                                                        '').replace('/link', '').replace('/', '')
     file = open(filename, 'wt+', encoding='utf8')
+    file.close()
     if 'https://' not in url and 'http://' not in url:  # 缺少协议头时自动补齐
         url = 'http://' + url
-    file.write(f'正在开始对 {url} 发起检查……\n')
+    with open(filename, 'a+', encoding='utf8') as file:
+        file.write(f'正在开始对 {url} 发起检查……\n')
     # 发现有些博客有检查user-agent，所以加上这个
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                             'Chrome/75.0.3770.142 Safari/537.36'}
@@ -65,7 +67,8 @@ def get_link(url, ss=False):
     error = ''
     import urllib3
     urllib3.disable_warnings()
-    file.write("检查开始了，你一共有 %s 个友链\n" % n)
+    with open(filename, 'a+', encoding='utf8') as file:
+        file.write("检查开始了，你一共有 %s 个友链\n" % n)
     for i in urls:
         x += 1
         friend_url = i
@@ -73,7 +76,8 @@ def get_link(url, ss=False):
         url2 = url1.split('.')
         url3 = url1 if len(url2) < 3 else url2[1] + "." + url2[2]
         if url3 in friend_url:
-            file.write((friend_url + f"：第 {str(x)} 个，自己就不检查了撒\n"))
+            with open(filename, 'a+', encoding='utf8') as file:
+                file.write((friend_url + f"：第 {str(x)} 个，自己就不检查了撒\n"))
         else:
             if ss is False:
                 # 自己输入就不用加/link路径了
@@ -87,28 +91,33 @@ def get_link(url, ss=False):
                 ret = requests.get(friend_url, headers=header,
                                    verify=False, timeout=60)
             except requests.exceptions.ConnectionError as e:
-                file.write(f'访问 {friend_url} 时出现了错误: {e}\n')
+                with open(filename, 'a+', encoding='utf8') as file:
+                    file.write(f'访问 {friend_url} 时出现了错误: {e}\n')
             if not ret.status_code == 200:
                 d += 1
                 error += friend_url + '\n'
-                file.write((f"第 {str(x)} 个，{friend_url} 链接有误，可能友链不是link，进度：" +
-                            str(round(x / n * 100, 1)) + "%") + '\n')
+                with open(filename, 'a+', encoding='utf8') as file:
+                    file.write((f"第 {str(x)} 个，{friend_url} 链接有误，可能友链不是link，进度：" +
+                                str(round(x / n * 100, 1)) + "%") + '\n')
             else:
                 # soup = etree.HTML(ret.text)
                 # ffriend_url = soup.xpath('//*[@id="article-container"]//@href')
                 if url1 in str(ret.text):
                     d1 += 1
                     success += friend_url + '\n'
-                    file.write((f"第 {str(x)} 个，{friend_url} 添加我了，进度：" + str(round(x /
+                    with open(filename, 'a+', encoding='utf8') as file:
+                        file.write((f"第 {str(x)} 个，{friend_url} 添加我了，进度：" + str(round(x /
                                                                                   n * 100, 1)) + "%") + '\n')
                 else:
                     d2 += 1
                     none += friend_url + '\n'
-                    file.write((f"第 {str(x)} 个，{friend_url} 没有添加我，进度：" + str(round(x /
-                                                                                   n * 100, 1)) + "%") + '\n')
-    file.write('统计结果：\n链接有误的有%s个，其中添加了我的有%s个，没有添加我的有%s个\n' % (str(d), str(d1), str(
-        d2)) + '添加了我的：\n' + success + '未添加我的：\n' + none + '链接可能存在错误的：\n' + error)
-    file.close()
+                    with open(filename, 'a+', encoding='utf8') as file:
+                        file.write((f"第 {str(x)} 个，{friend_url} 没有添加我，进度：" + str(round(x /
+                                                                                    n * 100, 1)) + "%") + '\n')
+    with open(filename, 'a+', encoding='utf8') as file:
+        file.write('统计结果：\n链接有误的有%s个，其中添加了我的有%s个，没有添加我的有%s个\n' % (str(d), str(d1), str(
+            d2)) + '添加了我的：\n' + success + '未添加我的：\n' + none + '链接可能存在错误的：\n' + error)
+        file.close()
     file = open(filename, 'r', encoding='utf8')
     result = file.read()
     file.close()
