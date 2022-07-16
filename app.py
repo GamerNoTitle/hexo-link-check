c721b46aa9f1a1784ef15eea142e6159c5b7dcc3
GamerNoTitle <GamerNoTitle@outlook.com>
2022-07-16 11:45:24 +0800

    Moved to flask

---
+++ app.py
@@ -0,0 +1,76 @@
+import logging
+import re
+from django.shortcuts import render
+from flask import Flask, send_from_directory, Blueprint, render_template
+from flask import request, redirect, abort, Response
+from flask_analytics import Analytics
+import flask
+import json
+import os
+import requests as r
+
+from utils.HexoLinkCheck import StartCheck
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('./index.html', status='')
+
+@app.route('/js/<path>', methods=['GET', 'POST'])
+def js(path):
+    return send_from_directory('js', path)
+
+@app.route('/css/<path>', methods=['GET', 'POST'])
+def css(path):
+    return send_from_directory('css', path)
+
+@app.route('/api/check', methods=['GET', 'POST'])
+def check():
+    try:
+        data = request.get_json()['content']
+        domain = data['domain']
+        path = data['path']
+    except:
+        data = request.form.to_dict()
+        domain = data['domain']
+        path = data['path']
+        if domain == None or path == None:
+            return {'code': -1, 'msg': 'Invalid parameters.'}
+    url = domain + path
+    data = StartCheck(url)
+    msg = data['msg']
+    return render_template('index.html', status=msg)
+
+
+@app.route('/report', methods=['GET', 'POST'])
+def report():
+    data = report_api()
+    return render_template('report.html', report=data)
+
+@app.route('/api/report', methods=['GET', 'POST'])
+def report_api():
+    Query = True
+    try:
+        data = request.get_json()['content']
+        domain = data['domain']
+    except:
+        try:
+            domain = request.args.get('domain') if request.args.get('domain') != None else request.form.to_dict()['domain']
+            if domain == None:
+                return {'code': -1, 'msg': 'Invalid parameters.'}
+        except:
+            Query = False
+    if Query:
+        filename = 'hexo-link-check/' + domain.replace('http://', '').replace('https://',
+                                                        '').replace('/link', '').replace('/', '')
+        try:
+            with open(filename, 'r', encoding='utf8') as f:
+                content = f.read().split('\n')
+            return content
+        except:
+            return [f'未找到 {domain} 的检查报告！']
+    else:
+        return ['请在上面输入你的域名进行报告获取！']
+if __name__ == '__main__':  # Launcher
+    # If debug is set to True, every time when the file is saved the program will reload
+    app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ utils/HexoLinkCheck.py
@@ -0,0 +1,140 @@
+from flask import Blueprint, request
+import requests
+from lxml import etree
+import re
+from urllib import parse
+import logging
+import _thread
+
+blueprint = Blueprint('hexo_link_check', __name__,
+                      url_prefix='/hexo_link_check')
+
+
+def StartCheck(url):
+    try:
+        _thread.start_new_thread(get_link, (url,))
+        return {'code': 0, 'msg': '检查请求提交成功！请等待约两分钟（可能更久）后在报告查看页面进行查看！'}
+    except Exception as e:
+        return {'code': -1, 'msg': f'请求提交失败，得到了 {e} 错误！'}
+
+
+def get_link(url, ss=False):
+    """
+    检查自己的友链状态
+    :param url: 输入自己的博客友链
+    :param ss: 是从自己的博客友链获取还是自己添加去查询对方是否添加了自己，默认从自己博客获取
+    :return:
+    """
+    print(f'Start checking {url}')
+    filename = 'hexo-link-check/' + url.replace('http://', '').replace('https://',
+                                                                       '').replace('/link', '').replace('/', '')
+    file = open(filename, 'wt+', encoding='utf8')
+    # 发现有些博客有检查user-agent，所以加上这个
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                            'Chrome/75.0.3770.142 Safari/537.36'}
+    if ss is False:
+
+        ret = requests.get(url, headers=header).content.decode('utf8')
+        # print(ret)
+        soup = etree.HTML(ret)
+        # //*[@id="article-container"]/div/div[2]/div[1]/a
+        # //*[@id="article-container"]/div/div[4]/div[1]/a/div[2]title cf-friends-name
+        # //*[@id="article-container"]/div/div[1]/div[1]/a[1]/div[2]/span[1]
+        friend_url = soup.xpath('//*[@id="article-container"]//@href')
+        # friend_name = soup.xpath('//*[@id="article-container"]//div/a/div[@class="flink-item-name"]/text()')
+        # 名字位置都不一样 就不判断了
+        # friend_name = soup.xpath('//*[@id="article-container"]//span[@class="title"]/text()')
+        # print(friend_name)
+        urls = re.findall(
+            r'(?:http.?|ftp|file)://[-A-Za-z\d+&@#/%?=~_|!:,.;]+[-A-Za-z\d+&@#/%=~_|]', str(friend_url))
+        if not urls:
+            return "支持Butterfly主题，不魔改友链的；可能支持部分Hexo主题。但是既然弹了这个提示就说明不支持啦~"
+    else:
+        urls = ['想查对方是否有你在这输入对方的友链地址1', '想查对方是否有你在这输入对方的友链地址2']
+
+    n = len(urls)
+    x = 0
+    d = 0
+    d1 = 0
+    d2 = 0
+
+    success = ''
+    none = ''
+    error = ''
+    import urllib3
+    urllib3.disable_warnings()
+    file.write("检查开始了，你一共有 %s 个友链\n" % n)
+    for i in urls:
+        x += 1
+        friend_url = i
+        url1 = parse.urlparse(url).netloc
+        url2 = url1.split('.')
+        url3 = url1 if len(url2) < 3 else url2[1] + "." + url2[2]
+        if url3 in friend_url:
+            file.write((friend_url + f"：第 {str(x)} 个，自己就不检查了撒\n"))
+        else:
+            if ss is False:
+                # 自己输入就不用加/link路径了
+                if friend_url[len(friend_url) - 1:] == '/':
+                    friend_url += 'link/'
+                else:
+                    friend_url += '/link/'
+            # if 'https' in friend_url:
+            #     friend_url = 'http' + friend_url[5:]
+            try:
+                ret = requests.get(friend_url, headers=header,
+                                   verify=False, timeout=60)
+            except requests.exceptions.ConnectionError as e:
+                file.write(f'访问 {friend_url} 时出现了错误: {e}\n')
+            if not ret.status_code == 200:
+                d += 1
+                error += friend_url + '\n'
+                file.write((f"第 {str(x)} 个，{friend_url} 链接有误，可能友链不是link，进度：" +
+                            str(round(x / n * 100, 1)) + "%") + '\n')
+            else:
+                # soup = etree.HTML(ret.text)
+                # ffriend_url = soup.xpath('//*[@id="article-container"]//@href')
+                if url1 in str(ret.text):
+                    d1 += 1
+                    success += friend_url + '\n'
+                    file.write((f"第 {str(x)} 个，{friend_url} 添加我了，进度：" + str(round(x /
+                                                                                  n * 100, 1)) + "%") + '\n')
+                else:
+                    d2 += 1
+                    none += friend_url + '\n'
+                    file.write((f"第 {str(x)} 个，{friend_url} 没有添加我，进度：" + str(round(x /
+                                                                                   n * 100, 1)) + "%") + '\n')
+    file.write('统计结果：\n链接有误的有%s个，其中添加了我的有%s个，没有添加我的有%s个\n' % (str(d), str(d1), str(
+        d2)) + '添加了我的：\n' + success + '未添加我的：\n' + none + '链接可能存在错误的：\n' + error)
+    file.close()
+    file = open(filename, 'r', encoding='utf8')
+    result = file.read()
+    file.close()
+    return result
+
+
+def get_str_btw(s, f, b):
+    par = s.partition(f)
+    return (par[2].partition(b))[0][:]
+
+
+if __name__ == '__main__':
+    # 输入待检查的博客友链地址即可
+    url = 'https://bili33.top/link/'
+    get_link(url)
+
+
+def logger(log_level, log_file):
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
+    BASIC_FORMAT = "%(asctime)s:%(levelname)s:%(message)s"
+    DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter(BASIC_FORMAT, DATE_FORMAT)
+    console = logging.StreamHandler()  # 输出到控制台的handler
+    console.setFormatter(formatter)
+    console.setLevel(log_level)  # 也可以不设置，不设置就默认用logger的level
+    file = logging.FileHandler(log_file, encoding='utf-8')  # 输出到文件的handler
+    file.setFormatter(formatter)
+    logger.addHandler(console)
+    logger.addHandler(file)
+    return logger
